c8dd14904aa5512d7f4e5a268d747e388824c9ce
shubham singhk <shubhamsinghia160@gmail.com>
2025-03-18 18:04:32 +0530

    good practice of js done

---
+++ practice/api.js
@@ -0,0 +1,494 @@
+[
+    {
+        "createdAt": "2025-03-17T23:31:45.850Z",
+        "name": "Jody Wyman",
+        "designation": "Dynamic Functionality Supervisor",
+        "joinDate": "1979-10-08T09:35:22.663Z",
+        "salary": "146.96",
+        "id": "1"
+    },
+    {
+        "createdAt": "2025-03-17T19:32:35.731Z",
+        "name": "Belinda MacGyver",
+        "designation": "Dynamic Security Associate",
+        "joinDate": "1988-04-24T01:48:15.653Z",
+        "salary": "496.96",
+        "id": "2"
+    },
+    {
+        "createdAt": "2025-03-17T16:33:18.538Z",
+        "name": "Lola Kassulke",
+        "designation": "Central Metrics Administrator",
+        "joinDate": "1988-02-23T16:25:36.757Z",
+        "salary": "730.70",
+        "id": "3"
+    },
+    {
+        "createdAt": "2025-03-17T08:10:14.534Z",
+        "name": "Annie Howe III",
+        "designation": "Senior Intranet Specialist",
+        "joinDate": "1975-05-13T07:11:25.569Z",
+        "salary": "22.56",
+        "id": "4"
+    },
+    {
+        "createdAt": "2025-03-17T20:58:04.588Z",
+        "name": "Betty Botsford",
+        "designation": "Internal Implementation Officer",
+        "joinDate": "1965-03-16T15:19:10.971Z",
+        "salary": "972.63",
+        "id": "5"
+    },
+    {
+        "createdAt": "2025-03-17T15:28:58.070Z",
+        "name": "Douglas Jakubowski",
+        "designation": "Future Functionality Developer",
+        "joinDate": "1989-03-23T14:22:16.382Z",
+        "salary": "862.31",
+        "id": "6"
+    },
+    {
+        "createdAt": "2025-03-17T19:28:37.839Z",
+        "name": "Flora Schulist",
+        "designation": "Customer Program Technician",
+        "joinDate": "2000-08-25T05:09:51.944Z",
+        "salary": "54.78",
+        "id": "7"
+    },
+    {
+        "createdAt": "2025-03-17T05:45:40.105Z",
+        "name": "June Gerhold",
+        "designation": "Customer Mobility Officer",
+        "joinDate": "1968-10-03T12:22:54.242Z",
+        "salary": "980.26",
+        "id": "8"
+    },
+    {
+        "createdAt": "2025-03-18T03:23:02.919Z",
+        "name": "Lillian Rutherford",
+        "designation": "Forward Program Director",
+        "joinDate": "1971-11-12T19:14:47.495Z",
+        "salary": "210.13",
+        "id": "9"
+    },
+    {
+        "createdAt": "2025-03-17T17:02:48.881Z",
+        "name": "Danielle Turner",
+        "designation": "Regional Accountability Architect",
+        "joinDate": "1996-09-16T20:36:51.770Z",
+        "salary": "817.27",
+        "id": "10"
+    },
+    {
+        "createdAt": "2025-03-17T04:54:17.290Z",
+        "name": "Juanita Dare",
+        "designation": "District Factors Executive",
+        "joinDate": "2002-04-29T17:08:16.271Z",
+        "salary": "572.25",
+        "id": "11"
+    },
+    {
+        "createdAt": "2025-03-17T07:57:31.895Z",
+        "name": "Tami Rolfson",
+        "designation": "Human Assurance Strategist",
+        "joinDate": "1990-06-15T03:44:07.232Z",
+        "salary": "404.87",
+        "id": "12"
+    },
+    {
+        "createdAt": "2025-03-17T11:49:22.322Z",
+        "name": "Rex Hickle",
+        "designation": "Chief Interactions Director",
+        "joinDate": "1957-06-14T02:05:51.517Z",
+        "salary": "284.01",
+        "id": "13"
+    },
+    {
+        "createdAt": "2025-03-17T06:26:54.370Z",
+        "name": "Paul Okuneva",
+        "designation": "Human Creative Coordinator",
+        "joinDate": "1970-09-07T13:05:00.143Z",
+        "salary": "762.56",
+        "id": "14"
+    },
+    {
+        "createdAt": "2025-03-18T03:04:57.826Z",
+        "name": "Joshua Williamson",
+        "designation": "Investor Response Planner",
+        "joinDate": "1955-05-15T21:05:39.126Z",
+        "salary": "894.98",
+        "id": "15"
+    },
+    {
+        "createdAt": "2025-03-17T16:18:51.994Z",
+        "name": "Roderick Beier Sr.",
+        "designation": "Chief Brand Director",
+        "joinDate": "1983-03-13T09:31:08.327Z",
+        "salary": "305.92",
+        "id": "16"
+    },
+    {
+        "createdAt": "2025-03-17T22:01:20.136Z",
+        "name": "Jerome Dare",
+        "designation": "Corporate Creative Consultant",
+        "joinDate": "1995-05-25T18:12:53.869Z",
+        "salary": "186.62",
+        "id": "17"
+    },
+    {
+        "createdAt": "2025-03-17T22:30:07.882Z",
+        "name": "Mrs. Wilbert Brown",
+        "designation": "Principal Program Developer",
+        "joinDate": "1989-05-22T04:10:22.317Z",
+        "salary": "798.55",
+        "id": "18"
+    },
+    {
+        "createdAt": "2025-03-17T07:57:57.589Z",
+        "name": "Lonnie Haley V",
+        "designation": "Investor Data Designer",
+        "joinDate": "1954-07-20T21:16:58.959Z",
+        "salary": "927.63",
+        "id": "19"
+    },
+    {
+        "createdAt": "2025-03-17T08:20:24.329Z",
+        "name": "Mandy Dickinson",
+        "designation": "National Optimization Strategist",
+        "joinDate": "1977-07-15T11:02:07.774Z",
+        "salary": "395.54",
+        "id": "20"
+    },
+    {
+        "createdAt": "2025-03-17T08:25:15.747Z",
+        "name": "Jared Jakubowski",
+        "designation": "Central Tactics Producer",
+        "joinDate": "1965-04-08T22:06:04.579Z",
+        "salary": "376.02",
+        "id": "21"
+    },
+    {
+        "createdAt": "2025-03-17T14:00:42.569Z",
+        "name": "Mrs. Irene Goldner",
+        "designation": "Principal Directives Technician",
+        "joinDate": "1986-11-11T18:40:26.248Z",
+        "salary": "159.08",
+        "id": "22"
+    },
+    {
+        "createdAt": "2025-03-17T23:18:01.393Z",
+        "name": "Rachael Bartoletti",
+        "designation": "Legacy Branding Analyst",
+        "joinDate": "1967-05-28T07:09:14.952Z",
+        "salary": "724.74",
+        "id": "23"
+    },
+    {
+        "createdAt": "2025-03-17T07:22:36.660Z",
+        "name": "Ervin Runolfsson",
+        "designation": "International Response Assistant",
+        "joinDate": "1962-11-15T14:47:38.257Z",
+        "salary": "380.47",
+        "id": "24"
+    },
+    {
+        "createdAt": "2025-03-17T14:25:02.566Z",
+        "name": "Karen Frami",
+        "designation": "Central Accountability Director",
+        "joinDate": "1977-07-14T06:25:23.990Z",
+        "salary": "718.07",
+        "id": "25"
+    },
+    {
+        "createdAt": "2025-03-17T14:21:09.297Z",
+        "name": "Nicole Green",
+        "designation": "National Solutions Director",
+        "joinDate": "1984-07-24T02:33:53.641Z",
+        "salary": "378.20",
+        "id": "26"
+    },
+    {
+        "createdAt": "2025-03-17T20:04:38.791Z",
+        "name": "Allison Robel",
+        "designation": "Principal Configuration Associate",
+        "joinDate": "1971-05-27T08:59:16.554Z",
+        "salary": "857.14",
+        "id": "27"
+    },
+    {
+        "createdAt": "2025-03-17T14:50:40.991Z",
+        "name": "Karla Cartwright",
+        "designation": "Global Communications Coordinator",
+        "joinDate": "1994-11-15T06:16:40.158Z",
+        "salary": "245.69",
+        "id": "28"
+    },
+    {
+        "createdAt": "2025-03-17T14:42:29.626Z",
+        "name": "Christy Mayert",
+        "designation": "Forward Division Engineer",
+        "joinDate": "1970-02-21T11:10:07.214Z",
+        "salary": "937.74",
+        "id": "29"
+    },
+    {
+        "createdAt": "2025-03-17T09:32:08.061Z",
+        "name": "Marsha Jenkins",
+        "designation": "Legacy Accounts Planner",
+        "joinDate": "1962-04-03T10:05:49.860Z",
+        "salary": "862.31",
+        "id": "30"
+    },
+    {
+        "createdAt": "2025-03-17T08:13:13.220Z",
+        "name": "Lynn Medhurst",
+        "designation": "Corporate Quality Supervisor",
+        "joinDate": "2007-12-25T18:40:39.902Z",
+        "salary": "475.84",
+        "id": "31"
+    },
+    {
+        "createdAt": "2025-03-18T04:02:39.237Z",
+        "name": "Dr. Francisco Schmeler",
+        "designation": "District Program Strategist",
+        "joinDate": "1975-11-29T20:19:19.806Z",
+        "salary": "603.82",
+        "id": "32"
+    },
+    {
+        "createdAt": "2025-03-18T03:31:31.297Z",
+        "name": "Ms. Homer Schulist",
+        "designation": "Investor Assurance Manager",
+        "joinDate": "2004-03-23T23:07:34.762Z",
+        "salary": "513.17",
+        "id": "33"
+    },
+    {
+        "createdAt": "2025-03-17T17:59:02.745Z",
+        "name": "Harold Conroy DVM",
+        "designation": "Future Factors Liaison",
+        "joinDate": "1986-11-30T14:51:46.838Z",
+        "salary": "925.47",
+        "id": "34"
+    },
+    {
+        "createdAt": "2025-03-17T11:50:16.642Z",
+        "name": "Priscilla Kunde Jr.",
+        "designation": "Forward Security Planner",
+        "joinDate": "1979-09-10T23:54:49.347Z",
+        "salary": "45.25",
+        "id": "35"
+    },
+    {
+        "createdAt": "2025-03-17T04:18:25.535Z",
+        "name": "Gene Harvey",
+        "designation": "Corporate Directives Officer",
+        "joinDate": "1988-11-24T05:24:32.708Z",
+        "salary": "834.38",
+        "id": "36"
+    },
+    {
+        "createdAt": "2025-03-17T13:38:58.488Z",
+        "name": "Donnie Kilback",
+        "designation": "Internal Division Assistant",
+        "joinDate": "2002-02-09T19:13:37.828Z",
+        "salary": "730.88",
+        "id": "37"
+    },
+    {
+        "createdAt": "2025-03-17T20:04:47.682Z",
+        "name": "Clyde Hirthe",
+        "designation": "Lead Creative Agent",
+        "joinDate": "1980-11-05T08:00:00.480Z",
+        "salary": "58.95",
+        "id": "38"
+    },
+    {
+        "createdAt": "2025-03-17T11:19:18.135Z",
+        "name": "Salvatore Bruen",
+        "designation": "Global Communications Technician",
+        "joinDate": "1954-10-09T12:59:31.900Z",
+        "salary": "1.86",
+        "id": "39"
+    },
+    {
+        "createdAt": "2025-03-18T00:01:05.790Z",
+        "name": "Edmund Rodriguez",
+        "designation": "International Factors Administrator",
+        "joinDate": "1947-12-16T11:43:53.782Z",
+        "salary": "449.80",
+        "id": "40"
+    },
+    {
+        "createdAt": "2025-03-17T23:50:40.123Z",
+        "name": "Curtis Wunsch Sr.",
+        "designation": "Senior Branding Architect",
+        "joinDate": "1946-10-14T00:34:51.404Z",
+        "salary": "2.61",
+        "id": "41"
+    },
+    {
+        "createdAt": "2025-03-17T04:56:39.025Z",
+        "name": "Beth Sipes",
+        "designation": "Human Tactics Director",
+        "joinDate": "1962-05-18T21:27:12.690Z",
+        "salary": "575.73",
+        "id": "42"
+    },
+    {
+        "createdAt": "2025-03-17T19:31:43.262Z",
+        "name": "Ms. Christian Fay",
+        "designation": "Product Metrics Officer",
+        "joinDate": "2005-06-25T00:00:40.453Z",
+        "salary": "755.65",
+        "id": "43"
+    },
+    {
+        "createdAt": "2025-03-18T03:56:25.054Z",
+        "name": "Miss Sandra Little",
+        "designation": "Legacy Directives Associate",
+        "joinDate": "2000-11-15T09:24:45.340Z",
+        "salary": "177.26",
+        "id": "44"
+    },
+    {
+        "createdAt": "2025-03-17T06:46:24.096Z",
+        "name": "Molly Dibbert",
+        "designation": "Product Creative Architect",
+        "joinDate": "1970-03-18T18:06:26.953Z",
+        "salary": "76.72",
+        "id": "45"
+    },
+    {
+        "createdAt": "2025-03-17T16:47:39.727Z",
+        "name": "Valerie Hettinger DVM",
+        "designation": "Principal Factors Facilitator",
+        "joinDate": "1947-05-04T23:58:42.530Z",
+        "salary": "315.63",
+        "id": "46"
+    },
+    {
+        "createdAt": "2025-03-18T00:09:29.144Z",
+        "name": "Mr. Willie Stroman",
+        "designation": "Dynamic Data Engineer",
+        "joinDate": "1956-09-19T09:10:18.853Z",
+        "salary": "932.62",
+        "id": "47"
+    },
+    {
+        "createdAt": "2025-03-17T13:44:46.564Z",
+        "name": "Bennie VonRueden",
+        "designation": "Chief Creative Administrator",
+        "joinDate": "1947-02-07T23:52:39.105Z",
+        "salary": "376.14",
+        "id": "48"
+    },
+    {
+        "createdAt": "2025-03-17T11:18:09.822Z",
+        "name": "Jamie Ritchie",
+        "designation": "International Program Designer",
+        "joinDate": "2002-07-06T22:47:55.847Z",
+        "salary": "904.53",
+        "id": "49"
+    },
+    {
+        "createdAt": "2025-03-17T19:28:51.146Z",
+        "name": "Chester Olson",
+        "designation": "Dynamic Infrastructure Technician",
+        "joinDate": "1974-08-10T16:15:50.314Z",
+        "salary": "754.27",
+        "id": "50"
+    },
+    {
+        "createdAt": "2025-03-18T04:08:20.865Z",
+        "name": "Brandon Jacobi",
+        "designation": "Human Markets Producer",
+        "joinDate": "1956-01-02T11:42:40.959Z",
+        "salary": "491.38",
+        "id": "51"
+    },
+    {
+        "createdAt": "2025-03-18T01:15:45.183Z",
+        "name": "Rudy Reichert",
+        "designation": "Human Solutions Engineer",
+        "joinDate": "1987-01-18T22:36:10.688Z",
+        "salary": "340.13",
+        "id": "52"
+    },
+    {
+        "createdAt": "2025-03-17T11:55:54.158Z",
+        "name": "Tammy Daniel MD",
+        "designation": "Investor Interactions Liaison",
+        "joinDate": "1998-05-27T02:54:12.214Z",
+        "salary": "834.23",
+        "id": "53"
+    },
+    {
+        "createdAt": "2025-03-17T04:14:59.995Z",
+        "name": "Ana Jacobs",
+        "designation": "Corporate Research Coordinator",
+        "joinDate": "1969-01-02T05:54:39.849Z",
+        "salary": "826.17",
+        "id": "54"
+    },
+    {
+        "createdAt": "2025-03-17T18:11:55.523Z",
+        "name": "Jake Collins",
+        "designation": "Senior Research Engineer",
+        "joinDate": "1995-02-24T05:10:47.588Z",
+        "salary": "929.61",
+        "id": "55"
+    },
+    {
+        "createdAt": "2025-03-17T10:41:26.990Z",
+        "name": "Leslie Labadie",
+        "designation": "Dynamic Security Analyst",
+        "joinDate": "1951-04-19T09:57:52.772Z",
+        "salary": "766.36",
+        "id": "56"
+    },
+    {
+        "createdAt": "2025-03-17T09:54:53.619Z",
+        "name": "Mathew Rosenbaum",
+        "designation": "International Integration Manager",
+        "joinDate": "1968-11-22T05:00:43.612Z",
+        "salary": "950.63",
+        "id": "57"
+    },
+    {
+        "createdAt": "2025-03-17T19:20:12.973Z",
+        "name": "Martin Mills",
+        "designation": "Direct Quality Associate",
+        "joinDate": "1978-03-22T12:25:21.090Z",
+        "salary": "787.51",
+        "id": "58"
+    },
+    {
+        "createdAt": "2025-03-17T17:41:51.067Z",
+        "name": "Debbie Green",
+        "designation": "Dynamic Paradigm Producer",
+        "joinDate": "1976-05-31T00:47:43.070Z",
+        "salary": "474.34",
+        "id": "59"
+    },
+    {
+        "createdAt": "2025-03-18T07:04:03.583Z",
+        "name": "Shubham singh",
+        "designation": "District Metrics Strategist",
+        "joinDate": "1987-03-04T21:07:19.828Z",
+        "salary": "475.24",
+        "id": "60",
+        "age": 23,
+        "gender": "male"
+    },
+    {
+        "createdAt": "2025-03-17T20:27:47.891Z",
+        "name": "Rahul",
+        "designation": "Chief Security Representative",
+        "joinDate": "1999-08-24T09:52:55.925Z",
+        "salary": "458.41",
+        "id": "61",
+        "age": 23,
+        "gender": "male"
+    }
+]
